36e4f3acb94e135b669f17596e8cf737bc6c552f
Pranav Donepudi <donepudipranav04@gmail.com>
2025-08-19 14:23:52 -0700

    render

--- main.py
+++ main.py
@@ -145,7 +145,5 @@ def experience_page():
     return render_template("experience.html", experience=experience)
 
 
-"""
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5500, debug=True)
-"""
