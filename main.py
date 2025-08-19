ab0a779fb786d83ec1d0961c9cd3d0315ddd5710
Pranav Donepudi <donepudipranav04@gmail.com>
2025-08-19 12:28:54 -0700

    vercel.json

--- main.py
+++ main.py
@@ -145,5 +145,7 @@ def experience_page():
     return render_template("experience.html", experience=experience)
 
 
+"""
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5500, debug=True)
+"""
